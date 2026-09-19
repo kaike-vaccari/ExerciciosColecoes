560247104c6514a0fa13b6247c75c241bca4b8b9
Kaike Vaccari do Nascimento <kaikevaccari78@gmail.com>
2026-09-19 19:45:36 -0300

    add: Ex02

---
+++ src/exercicio02/Ex02.java
@@ -0,0 +1,57 @@
+package exercicio02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex02 {
+	
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+//		2 - Faça um programa que preencha uma LinkedList com
+//		números inteiros (ele deve parar de armazenar quando
+//		digitar zero), mostre a lista original na tela, peça ao usuário
+//		um número e mostre na tela a quantidade de vezes esse
+//		número aparece na lista
+		ArrayList<Integer> lista = new ArrayList<>();
+		int num;
+		
+		preencher(lista);
+		System.out.println("Lista original: \n"+lista);
+		
+		System.out.println("Digite um número para procurar na lista: ");
+		num = scan.nextInt();
+		
+		int ocorrencias = contarOcorrencia(lista, num);
+		
+		System.out.println("O número de vezes que o número aparece é: "+ ocorrencias);
+		
+	}
+
+	private static void preencher(ArrayList<Integer> lista) {
+		int num;
+		
+		do {
+			System.out.println("Número para adicionar à lista: ");
+			num = scan.nextInt();
+			lista.add(num);
+			
+		}while(num!=0);
+		
+	}
+
+	private static int contarOcorrencia(ArrayList<Integer> lista, int num) {
+		int ocorrencias = 0;
+		
+		for(int i=0;i<lista.size();i++) {
+			if(lista.get(i)==num) {
+				ocorrencias++; // Conta as occorencias do número na lista
+			}
+		}
+		if(ocorrencias==0) {
+			return -1; // Retorna -1 caso não encontre nenhum número de ocorrencias no vetor
+		}
+		return ocorrencias;
+	}
+
+}
